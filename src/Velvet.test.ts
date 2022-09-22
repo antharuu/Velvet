b70dd01f8188f777d14e4cfc28cc4dc0937efef5
Anthony <antho.eclips@gmail.com>
2022-09-22 13:14:53 +0200

    feat: trim & ignore empty lignes

--- src/Velvet.test.ts
+++ src/Velvet.test.ts
@@ -3,3 +3,15 @@ import Velvet from "./Velvet";
 test("Parse simple tag", () => {
   expect(Velvet.parse("h1 Hello world")).toBe("<h1>Hello world</h1>");
 });
+
+test("Remove empty lines", () => {
+  expect(
+    Velvet.parse(`
+  h1 Hello world
+  
+  h2 How are you ? 
+  
+  `)
+  ).toBe(`<h1>Hello world</h1>
+<h2>How are you ?</h2>`);
+});
