b564cc289934d07ed65f7c71b651886f6cfe3e7e
Anthony <antho.eclips@gmail.com>
2022-09-26 23:21:18 +0200

    chore: restart

--- src/Velvet/Parser.test.ts
+++ src/Velvet/Parser.test.ts
@@ -1,3 +1,8 @@
-test(() => {
-	expect(1).toBe(1);
+import Parser from "./Parser";
+import { AST } from "./Types/AST";
+
+test.todo("should return a simple block", () => {
+	expect(Parser.convert(`h1 Hello`)).toStrictEqual([
+		{ tag: "h1", children: ["Hello"], indent: 0 },
+	] as AST);
 });
--- src/Velvet/Parser.ts
+++ src/Velvet/Parser.ts
@@ -1,4 +1,19 @@
+import { getBlocksOf, getLinesOf } from "./Tools";
+import { AST, VNode } from "./Types/AST";
+
 /**
- * Parse the Velvet code into AST
+ * Parse the Velvet code into AST *(Abstract Syntax Tree)*
  */
-export default class Parser {}
+export default class Parser {
+	/**
+	 * Convert a string into an usable AST
+	 * @param velvetCode String to be converted in AST
+	 */
+	static convert(velvetCode: string): AST {
+		const ast: AST = [];
+
+		// getBlocksOf(velvetCode).forEach((line: string) => {});
+
+		return ast;
+	}
+}
